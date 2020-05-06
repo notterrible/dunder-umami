e1f68dd1abdb4dab7ea29dbb2f54b41e6b75809a
Raymond Wang <raymond.wang@pantheon.io>
2020-05-06 13:56:18 -0700

    modifications from ddev

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,3 +23,8 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+// Automatically generated include for settings managed by ddev.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
